119770e7cda05d37987f06c34faf843c7fb5b2b2
Augusto Ojeda <okarinex@gmail.com>
2022-08-31 16:54:44 -0300

    generacion de tickets done

--- sockets/controller.js
+++ sockets/controller.js
@@ -11,7 +11,10 @@ const socketController = (socket) =>{
         console.log('cliente desconectado del server', socket.id)
     })
 
+    // Eventos cuando un cliente se conecta
     socket.emit('ultimo-ticket', ticketControl.ultimo)
+    socket.emit('estado-actual', ticketControl.ultimos4)
+    socket.emit('tickets-pendientes', ticketControl.tickets.length )
 
 
     socket.on('siguiente-ticket', (payload, callback)=>{
@@ -21,6 +24,36 @@ const socketController = (socket) =>{
         callback(siguiente)
 
         // TODO: Notificar que hay nuevo ticket pendiente de asignar
+    })
+
+    socket.on('atender-ticket', ({escritorio}, callback)=>{
+
+
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4)
+        socket.emit('tickets-pendientes', ticketControl.tickets.length )
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length )
+
+        if(!escritorio){
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            })
+        }
+
+        const ticket = ticketControl.atenderTicket( escritorio )
+
+        if(!ticket){
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            })
+        }else{
+            callback({
+                ok: true,
+                ticket
+            })
+        }
+
     })
 
 }
